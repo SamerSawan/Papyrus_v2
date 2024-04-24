c6988766ad92137bd2fd1aa76754c4e527f7a496
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-24 16:06:02 -0400

    buggy code

---
+++ lib/description_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Center(
+  child: const Container(
+    margin: const EdgeInsets.all(10.0),
+    color: Colors.amber[600],
+    width: 48.0,
+    height: 48.0,
+  ),
+)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'description_box.dart';
 
 void main() {
   runApp(MaterialApp(
