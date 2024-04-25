50af9d348332637d933f8f2576a9558d313a9953
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-24 22:33:42 -0400

    description and information boxes for book clubs

--- lib/description_box.dart
+++ lib/description_box.dart
@@ -1,26 +1,112 @@
 import 'package:flutter/material.dart';
 
-
 class DescriptionBox extends StatelessWidget {
   const DescriptionBox({super.key});
 
   @override
   Widget build(context) {
-    return Center(
-    child:  Column (children: [
-    const Text("Book club description"),
-    Positioned(
-      top: 251,
-      left: 20,
-      right: null,
-      bottom: null, 
+    return Align(
+      alignment: const AlignmentDirectional(0, 0.67),
       child: Container(
-   decoration: BoxDecoration(
+        width: 367,
+        height: 90,
+        decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left:0, right: 3.5),
+              width: 180,
+              height: 90,
+              decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0x8053917E)
+              ),
+              child: const Center (
+                child: Text(
+          'Description',
+          style: TextStyle(
+            color: Color.fromARGB(255, 245, 245, 221),
+            fontFamily: 'Inter',
+            fontSize: 12,
+          )
+          ),
+            ) 
+            ),
+            Container(
+              margin: const EdgeInsets.only(left:3.5, right: 0),
+              width: 180,
+              height: 90,
+              decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0x8053917E)
+              ),
+              child: const Center (
+                child: Text(
+          'Information',
+          style: TextStyle(
+            color: Color.fromARGB(255, 245, 245, 221),
+            fontFamily: 'Inter',
+            fontSize: 12,
+          )
+          ),
+            ),
+            )
+          ],
+        ),
+      ),
+      );
+     }
+}
+    
+    /* Row(
+       mainAxisSize: MainAxisSize.max,
+  children: [
+    Container(
+      width: 175,
+      height: 90,
+      decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0x8053917E)
+      ),
+    ),
+    Container(
+      width: 175,
+      height: 90,
+      decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0x8053917E)
+      ),
+    ),
+  ],
+    );
+  }
+}
+
+
+   children:  [   
+      Positioned(
+      left: 20,
+      top: 21, 
+      child: Container(decoration: BoxDecoration(
    borderRadius: BorderRadius.circular(6),
    color: const Color(0x8053917E)),
-   height: 100,
-   width: 100,
- )) ],)
-);
+   height: 90,
+   width: 175,),
+ ),
+ Positioned(
+      left: 203,
+      bottom: 251, 
+      child: Container(decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0x8053917E)),
+   height: 90,
+   width: 175,)
+      
+ )
+ ],) 
+    ;
 }
-}
+} */
