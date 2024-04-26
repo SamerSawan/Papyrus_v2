826bba32138f29306acf7e78dce42855910e04d4
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-26 18:11:44 -0400

    finished nav bar

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,36 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:papyrus/book_timeline.dart';
-import 'package:papyrus/reading_progress.dart';
-import 'description_box.dart';
 
-class NavigationBar extends StatelessWidget {
+class NavigationBar extends StatefulWidget {
   const NavigationBar({super.key});
 
   @override
-  Widget build(context) {
+  State<NavigationBar> createState() => _NavigationBar();
+}
+
+
+class _NavigationBar extends State<NavigationBar> {
+int _currentIndex=0;
+List<Widget> body = const [
+  Icon(Icons.home),
+  Icon(Icons.book_online),
+  Icon(Icons.compass_calibration),
+  Icon(Icons.read_more),
+  Icon(Icons.person)
+];
+  @override
+  Widget build(context){
     return Scaffold(
-     bottomNavigationBar: SafeArea(
-  child: SizedBox(
-    height:10,
-   child: BottomNavigationBar(
-    items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: Icon(Icons.call)
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.camera)
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.chat)
-      ),
-    ],
-  )
-  )
-  ),
-);
+      body: Center(
+        child: body[_currentIndex],
+        ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+            border: Border(top: BorderSide(color: Color(0xFFD2F1E4), width: 1)
+            )
+            ),
+      child: BottomNavigationBar(
+        iconSize: 25,
+        selectedItemColor: const Color(0xFFD2F1E4),
+        unselectedItemColor: const Color.fromARGB(255, 125, 125, 125),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+backgroundColor: Color(0x80001A23),
+            label: 'Home',
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0x80001A23),
+            label: 'Books',
+            icon: Icon(Icons.auto_stories)
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0x80001A23),
+            label: 'Discover',
+            icon: Icon(Icons.explore)
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Color(0x80001A23),
+            label: 'Club',
+            icon: Icon(Icons.local_library)
+            ),
+          BottomNavigationBarItem(
+            backgroundColor: Color(0x80001A23),
+            label: 'Profile',
+            icon: Icon(Icons.person)
+          )
+        ],
+      )
+    ),
+    );
   }
 }
- 
 
 void main() {
   runApp(MaterialApp(
@@ -38,35 +79,9 @@ void main() {
       scaffoldBackgroundColor: const Color(0xFF001A23),
     ),
     home: const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [BookCard(), DescriptionBox(), ReadingProgress(), BookTimeline(), NavigationBar()
-        ],
+      body: NavigationBar()
+        ,
       ),
     ),
-  ));
-}
-
-class BookCard extends StatelessWidget {
-  const BookCard({super.key});
-
-  @override
-  Widget build(context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-              margin: const EdgeInsets.all(10.0),
-              width: 367.0,
-              height: 150.0,
-              decoration: BoxDecoration(
-   borderRadius: BorderRadius.circular(6),
-   color: const Color(0xFF53917E).withOpacity(0.5),
-              )
-              ),
-        ],
-      ),
-    );
-  }
+  );
 }
