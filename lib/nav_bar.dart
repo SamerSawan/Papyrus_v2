fa91a90566cc5ab1c5275e925404a7eb645f8516
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-26 13:43:12 -0400

    nav bar

---
+++ lib/nav_bar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:papyrus/book_timeline.dart';
+import 'package:papyrus/reading_progress.dart';
+import 'description_box.dart';
+
+class NavigationBar extends StatelessWidget {
+  const NavigationBar({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+     bottomNavigationBar: SafeArea(
+  child: SizedBox(
+    height:10,
+   child: BottomNavigationBar(
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.call)
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.camera)
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.chat)
+      ),
+    ],
+  )
+  )
+  ),
+);
+  }
+}
+ 
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      scaffoldBackgroundColor: const Color(0xFF001A23),
+    ),
+    home: const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [BookCard(), DescriptionBox(), ReadingProgress(), BookTimeline(), NavigationBar()
+        ],
+      ),
+    ),
+  ));
+}
+
+class BookCard extends StatelessWidget {
+  const BookCard({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        children: [
+          Container(
+              margin: const EdgeInsets.all(10.0),
+              width: 367.0,
+              height: 150.0,
+              decoration: BoxDecoration(
+   borderRadius: BorderRadius.circular(6),
+   color: const Color(0xFF53917E).withOpacity(0.5),
+              )
+              ),
+        ],
+      ),
+    );
+  }
+}
