767a0d8a73ae03532fd769531d4820b57741db21
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-26 19:09:56 -0400

    added text to book card

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:papyrus/book_timeline.dart';
 import 'package:papyrus/reading_progress.dart';
 import 'description_box.dart';
-//import 'nav_bar.dart';
+import 'book_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -22,27 +22,4 @@ void main() {
       ),
     ),
   );
-}
-
-class BookCard extends StatelessWidget {
-  const BookCard({super.key});
-
-  @override
-  Widget build(context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-              margin: const EdgeInsets.all(10.0),
-              width: 367.0,
-              height: 150.0,
-              decoration: BoxDecoration(
-   borderRadius: BorderRadius.circular(6),
-   color: const Color(0xFF53917E).withOpacity(0.5),
-              )
-              ),
-        ],
-      ),
-    );
-  }
 }
