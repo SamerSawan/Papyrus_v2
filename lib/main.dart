d2b59679082f7552cd3d208f847c842396bc4aba
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-04-27 09:04:50 -0400

    fixed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/book_card.dart';
 import 'package:papyrus/book_timeline.dart';
 import 'package:papyrus/reading_progress.dart';
 import 'description_box.dart';
 import 'book_timeline.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      scaffoldBackgroundColor: const Color(0xFF001A23),
-    ),
-    home: Scaffold(
-      body: ListView(
-        children: const [Column(
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF001A23),
+      ),
+      home: Scaffold(
+          body: ListView(children: const [
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-          BookCard(), DescriptionBox(), ReadingProgress(), BookTimeline()
-        ], 
-      ),]
-      )
-      ),
+          children: [
+            BookCard(),
+            DescriptionBox(),
+            ReadingProgress(),
+            BookTimeline()
+          ],
+        ),
+      ])),
     ),
   );
 }
