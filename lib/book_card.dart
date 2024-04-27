151acf7acaff685e9032947a87d19942f1a4cb9e
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-04-27 10:26:38 -0400

    Book Card is now rendered via http request to google books

--- lib/book_card.dart
+++ lib/book_card.dart
@@ -1,82 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/api/book_service.dart';
+import 'package:papyrus/core/models/book.dart';
 
-class BookCard extends StatelessWidget {
+class BookCard extends StatefulWidget {
   const BookCard({super.key});
+
+  @override
+  State<BookCard> createState() => _BookCardState();
+}
+
+class _BookCardState extends State<BookCard> {
+  late Future<Book> futureBook;
+  BookService bookService = BookService();
+
+  @override
+  void initState() {
+    super.initState();
+    futureBook = bookService.searchBooks(bookTitle: 'Anna Karenina');
+  }
 
   @override
   Widget build(context) {
     return Center(
-      child: SizedBox (
-        width: 367,
-        height: 190,
-        child: Column(
-          children: [
-            const Text('Book Club Name',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
+        child: SizedBox(
+            width: 367,
+            height: 190,
+            child: Column(children: [
+              const Text('Book Club Name',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                       color: Color.fromRGBO(245, 245, 221, 1),
                       fontFamily: 'Inter',
                       fontSize: 16,
-                      fontWeight: FontWeight.bold
-                        )
-                        ),
-              const SizedBox(height:10),
+                      fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
               Container(
-              width: 367.0,
-              height: 150.0,
-              decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: const Color(0xFF53917E).withOpacity(0.5)
-              ),
-              child: const Row(
-                children: [SizedBox(width: 183.5), // replace sizedbox with book image
-                    Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height:21),
-                      Text('Title',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Color.fromRGBO(245, 245, 221, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold
-                        )
-                        ),
-                      Text('Author',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Color.fromRGBO(245, 245, 221, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w200
-                        )
-                        ),
-                      Text('Ratings',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Color.fromRGBO(245, 245, 221, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 10,
-                        )
-                        ),
-                      Text('Description',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Color.fromRGBO(245, 245, 221, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 10,
-                      fontWeight: FontWeight.w800
-                        )
-                        )     
-                        ]
-              )
-              ]
-              )
-              )
-              ]
-              )
-              )
-              );
+                  width: 367.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color(0xFF53917E).withOpacity(0.5)),
+                  child: FutureBuilder(
+                    future: futureBook,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final book = snapshot.data;
+                        if (book != null) {
+                          return Row(children: [
+                            const SizedBox(width: 15),
+                            Container(
+                              height: 127,
+                              width: 80,
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Image.network(book.image),
+                            ),
+
+                            const SizedBox(
+                                width: 15), // replace sizedbox with book image
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 21),
+                                  Text(book.title,
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                          color:
+                                              Color.fromRGBO(245, 245, 221, 1),
+                                          fontFamily: 'Inter',
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
+                                  Text(book.authors[0],
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                          color:
+                                              Color.fromRGBO(245, 245, 221, 1),
+                                          fontFamily: 'Inter',
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w200)),
+                                  const Text(
+                                    'Ratings',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(245, 245, 221, 1),
+                                      fontFamily: 'Inter',
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 230,
+                                    child: Text(
+                                      book.description,
+                                      textAlign: TextAlign.left,
+                                      softWrap: true,
+                                      maxLines: 4,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        color: Color.fromRGBO(245, 245, 221, 1),
+                                        fontFamily: 'Inter',
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                  ),
+                                ])
+                          ]);
+                        } else {
+                          return const Text('No Book Data');
+                        }
+                      } else if (snapshot.hasError) {
+                        return Text('$snapshot.error');
+                      }
+                      return const CircularProgressIndicator();
+                    },
+                  ))
+            ])));
   }
 }
