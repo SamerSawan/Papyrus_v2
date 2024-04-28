5ee5bbbd92eef221f2154ec38f5890ae053485f3
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-04-28 10:06:20 -0400

    information box widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/models/book.dart';
 import 'package:papyrus/core/models/book_club.dart';
-import 'package:papyrus/screens/read_along_screen/widgets/book_card.dart';
-import 'package:papyrus/screens/read_along_screen/widgets/book_timeline.dart';
+import 'package:papyrus/screens/widgets/book_card.dart';
+import 'package:papyrus/screens/widgets/book_timeline.dart';
 import 'package:papyrus/reading_progress.dart';
-import 'screens/read_along_screen/widgets/description_box.dart';
+import 'package:papyrus/screens/widgets/information_box.dart';
+import 'screens/widgets/description_box.dart';
 
 void main() {
   BookClub bookClub = BookClub(
@@ -32,9 +34,21 @@ void main() {
                     fontSize: 16,
                     fontWeight: FontWeight.bold)),
             const BookCard(),
-            DescriptionBox(
-              description: bookClub.description,
-            ),
+            Align(
+      alignment: const AlignmentDirectional(0, 0.67),
+      child: Container(
+        width: 367,
+        height: 90,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: const Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [DescriptionBox(description: bookClub.description), InformationBox()
+          ],
+        ),
+      ),
+    ),
             const ReadingProgress(),
             const BookTimeline()
           ],
--- lib/screens/widgets/description_box.dart
+++ lib/screens/widgets/description_box.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/models/book.dart';
+import 'package:papyrus/core/models/book_club.dart';
 
 class DescriptionBox extends StatelessWidget {
   final String description;
@@ -6,17 +8,7 @@ class DescriptionBox extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Align(
-      alignment: const AlignmentDirectional(0, 0.67),
-      child: Container(
-        width: 367,
-        height: 90,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
+    return 
             Container(
                 margin: const EdgeInsets.only(left: 0, right: 3.5),
                 width: 180,
@@ -30,27 +22,8 @@ class DescriptionBox extends StatelessWidget {
                         color: Color.fromARGB(255, 245, 245, 221),
                         fontFamily: 'Inter',
                         fontSize: 12,
-                      )),
-                )),
-            Container(
-              margin: const EdgeInsets.only(left: 3.5, right: 0),
-              width: 180,
-              height: 90,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6),
-                  color: const Color(0x8053917E)),
-              child: const Center(
-                child: Text('Information',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 245, 245, 221),
-                      fontFamily: 'Inter',
-                      fontSize: 12,
-                    )),
-              ),
-            )
-          ],
-        ),
-      ),
+                      ))
+                )
     );
   }
 }
