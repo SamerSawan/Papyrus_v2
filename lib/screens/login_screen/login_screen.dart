5948ff695cc8f40d3143501a8fbca52af50cc16c
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-04-30 00:24:30 -0400

    Added custom button and firebase login stuff

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,12 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:papyrus/firebase_options.dart';
+import 'package:papyrus/screens/widgets/custom_button.dart';
 import 'package:papyrus/screens/widgets/custom_text_input.dart';
 
-void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      scaffoldBackgroundColor: const Color(0xFF001A23),
-    ),
-    home: Scaffold(
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  // text controllers
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -29,13 +45,38 @@ void main() {
               ),
             ),
             const SizedBox(height: 20),
-            const CustomTextInput(label: "Username"),
-            const SizedBox(height: 20),
-            const CustomTextInput(label: "Password"),
-            const SizedBox(height: 12),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextInput(
+                    label: "Username",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      // You can add more validation logic here
+                      return null; // Return null if the input is valid
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  CustomTextInput(
+                    label: "Password",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      // You can add more validation logic here
+                      return null; // Return null if the input is valid
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                ],
+              ),
+            ),
             Text(
               // needs to be turned into a navigation button
-              "Forgot username or password",
+              "Forgot password?",
               style: TextStyle(
                 color: const Color(0xFFF5F5DD).withOpacity(0.5),
                 fontFamily: 'Inter',
@@ -43,20 +84,48 @@ void main() {
                 fontWeight: FontWeight.w200,
               ),
             ),
-            const SizedBox(height: 5),
-            const Text(
-              // needs to be turned into a navigation button
-              "Don't have an account? Create one here",
-              style: TextStyle(
-                color: Color(0xFFF5F5DD),
-                fontFamily: 'Inter',
-                fontSize: 14,
-                fontWeight: FontWeight.w200,
-              ),
+            const SizedBox(height: 10),
+            CustomButton(
+              text: "Sign In",
+              onTap: login,
+            ),
+            const SizedBox(height: 10),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account? ",
+                  style: TextStyle(
+                    color: Color(0xFFF5F5DD),
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                Text(
+                  "Register here",
+                  style: TextStyle(
+                    color: Color(0xFFF5F5DD),
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w200,
+                  ),
+                )
+              ],
             ),
           ],
         ),
       ),
-    ),
-  ));
+    );
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF001A23),
+      ),
+      home: const LoginScreen()));
 }
