e5a988979a235b91882c09abdfba4a02ab7426c0
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-06 23:57:39 -0400

    refactored some code, added firestore_service

--- lib/core/auth/auth.dart
+++ lib/core/auth/auth.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 
 import 'package:flutter/material.dart';
-import 'package:papyrus/auth/login_or_signup.dart';
+import 'package:papyrus/core/auth/login_or_signup.dart';
 import 'package:papyrus/real_nav_bar.dart';
 import 'package:papyrus/screens/my_book_clubs/my_book_clubs.dart';
 
