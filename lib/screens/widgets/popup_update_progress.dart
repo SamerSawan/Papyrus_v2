633f8021e5dda3adb82861f343339f5abd4c6b2e
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-07 14:40:38 -0400

    added functionality for posting comments to the database

--- lib/screens/widgets/popup_update_progress.dart
+++ lib/screens/widgets/popup_update_progress.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/services/firestore_service.dart';
 import 'package:papyrus/screens/widgets/custom_percentage_update.dart';
 import 'package:papyrus/screens/widgets/custom_text_update.dart';
 
 void main() {
   runApp(
-  const MaterialApp( 
-      home: Scaffold(body: Center(child: TestPop()))));}
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: TestPop(),
+        ),
+      ),
+    ),
+  );
+}
 
 class TestPop extends StatefulWidget {
   const TestPop({super.key});
@@ -18,89 +26,97 @@ class TestPop extends StatefulWidget {
 class _TestPopState extends State<TestPop> {
   final _formKey = GlobalKey<FormState>();
 
+  final FirestoreService firestoreService = FirestoreService();
+
   TextEditingController progressController = TextEditingController();
   TextEditingController commentController = TextEditingController();
 
-
   @override
   Widget build(context) {
-  return Container(
-          margin: const EdgeInsets.only(bottom: 40),
-          child: ElevatedButton(
-            style: const ButtonStyle(
-              backgroundColor:MaterialStatePropertyAll(Color(0xFFD2F1E4))),
-            onPressed: () async {
-              await showDialog<void>(
-                  context: context,
-                  builder: (context) => AlertDialog(
+    return Container(
+      margin: const EdgeInsets.only(bottom: 40),
+      child: ElevatedButton(
+        style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Color(0xFFD2F1E4))),
+        onPressed: () async {
+          await showDialog<void>(
+              context: context,
+              builder: (context) => AlertDialog(
                     contentPadding: const EdgeInsets.all(10),
-                    shape:RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(11)),
-                    backgroundColor:const Color.fromARGB(255, 210, 241, 228),
-                        content: Form(
-                          key: _formKey,
-                          child: Stack(
-                           clipBehavior: Clip.none,
-                            children: [
-                              SizedBox(
-                                height:215,
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children:[
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children:[
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(11)),
+                    backgroundColor: const Color.fromARGB(255, 210, 241, 228),
+                    content: Form(
+                      key: _formKey,
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: [
+                          SizedBox(
+                            height: 215,
+                            child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
                                         TextButton(
                                           onPressed: () {
-                                          Navigator.of(context).pop();
+                                            Navigator.of(context).pop();
                                           },
                                           child: const Text('Cancel',
-                                            style: TextStyle(
-                                            fontSize: 15,
-                                            color: Color.fromARGB(255, 0, 0, 0),
-                                          )
-                                    ),
-                                  ),
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Color.fromARGB(
+                                                    255, 0, 0, 0),
+                                              )),
+                                        ),
                                         TextButton(
                                           child: const Text('Submit',
-                                            style: TextStyle(
-                                              fontSize: 15,
-                                              color: Color.fromARGB(255, 0, 0, 0),
-                                          )),
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Color.fromARGB(
+                                                    255, 0, 0, 0),
+                                              )),
                                           onPressed: () {
-                                            if (_formKey.currentState!.validate()) {
-                                              _formKey.currentState!.save();
-                                              Navigator.of(context).pop();
-                                            }
+                                            firestoreService.addComment(
+                                              commentController.text,
+                                            );
+
+                                            commentController.clear();
+                                            Navigator.pop(context);
                                           },
-                                        )]
-                                  ),
-                                  const Text('Check in with your reading progress.', 
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold
-                                  )),
+                                        )
+                                      ]),
+                                  const Text(
+                                      'Check in with your reading progress.',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold)),
                                   const SizedBox(
                                     height: 10,
                                   ),
                                   Row(
                                     children: [
                                       const Text('I\'m ',
-                                      style: TextStyle(
-                                        color: Color.fromARGB(194, 0, 0, 0),
-                                        fontSize: 13,
-                                    )),
-                                  CustomPercentageUpdate(
-                                      label: '',
-                                      controller: progressController, 
-                                      validator: (value) {return null;}
-                                      ),
-                                  const Text(' % done.', //through instead of done ??
-                                    style: TextStyle(
-                                      color: Color.fromARGB(194, 0, 0, 0),
-                                      fontSize: 13,
-                                    )),
+                                          style: TextStyle(
+                                            color: Color.fromARGB(194, 0, 0, 0),
+                                            fontSize: 13,
+                                          )),
+                                      CustomPercentageUpdate(
+                                          label: '',
+                                          controller: progressController,
+                                          validator: (value) {
+                                            return null;
+                                          }),
+                                      const Text(
+                                          ' % done.', //through instead of done ??
+                                          style: TextStyle(
+                                            color: Color.fromARGB(194, 0, 0, 0),
+                                            fontSize: 13,
+                                          )),
                                     ],
                                   ),
                                   const SizedBox(
@@ -108,37 +124,37 @@ class _TestPopState extends State<TestPop> {
                                   ),
                                   const Align(
                                     alignment: AlignmentDirectional.centerStart,
-                                    child: Text('What do you think so far?', 
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                      color: Color.fromARGB(194, 0, 0, 0),
-                                      fontSize: 13,
-                                    )),
+                                    child: Text('What do you think so far?',
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(
+                                          color: Color.fromARGB(194, 0, 0, 0),
+                                          fontSize: 13,
+                                        )),
                                   ),
                                   Column(
                                     mainAxisSize: MainAxisSize.max,
-                                    children: [CustomTextUpdate(
-                                      label: 'Add a comment...',
-                                      controller: commentController, 
-                                      validator: (value) {return null;}
-                                      ),
+                                    children: [
+                                      CustomTextUpdate(
+                                          label: 'Add a comment...',
+                                          controller: commentController,
+                                          validator: (value) {
+                                            return null;
+                                          }),
                                     ],
                                   ),
-                                  ]
-                                ),
-                              ),
-                            ],
+                                ]),
                           ),
-                        ),
-                      ));
-            },
-            child: const Text('Update Progress',
+                        ],
+                      ),
+                    ),
+                  ));
+        },
+        child: const Text('Update Progress',
             style: TextStyle(
-              color: Color.fromARGB(255, 0, 0, 0),
-              fontSize: 14,
-              fontWeight: FontWeight.bold
-            )),
-          ),
-        );
-}
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 14,
+                fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
 }
