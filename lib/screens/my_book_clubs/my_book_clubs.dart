5676bd30880d35c950a18f2587187065a00e42b4
Joanna Chattali <joanna@MacBook-Air-de-Joanna.local>
2024-05-08 17:24:09 -0400

    made a comment widget

--- lib/screens/my_book_clubs/my_book_clubs.dart
+++ lib/screens/my_book_clubs/my_book_clubs.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:papyrus/core/models/book_club.dart';
 import 'package:papyrus/screens/book_club_screen/book_club_screen.dart';
+//import 'package:papyrus/screens/choose_book_screen/choose_book_screen.dart';
 import 'package:papyrus/screens/widgets/book_club_card.dart';
+import 'package:papyrus/screens/widgets/custom_text_update.dart';
 import 'package:papyrus/screens/widgets/search_bar_clubs.dart';
 
 BookClub bookClub = BookClub(
@@ -13,10 +15,16 @@ BookClub bookClub = BookClub(
   users: [],
 );
 
-class MyBookClubs extends StatelessWidget {
+class MyBookClubs extends StatefulWidget {
   const MyBookClubs({super.key});
 
-  void displayAddBookClub() {}
+  @override
+  State<MyBookClubs> createState() => _MyBookClubsState();
+}
+
+class _MyBookClubsState extends State<MyBookClubs> {
+  TextEditingController clubNameController = TextEditingController();
+  TextEditingController clubDescriptionController = TextEditingController();
 
   void logout() {
     FirebaseAuth.instance.signOut();
@@ -27,14 +35,97 @@ class MyBookClubs extends StatelessWidget {
     return Scaffold(
       body: ListView(children: [
         CupertinoNavigationBar(
-          middle: Text('My Book Clubs',
-              style: Theme.of(context).textTheme.titleMedium,
-              ),
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          middle: const Text(
+            'My Book Clubs',
+            style: TextStyle(
+              color: Color(0xFFF5F5DD),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          backgroundColor: const Color(0xFF001A23),
           leading:
               IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
           trailing: IconButton(
-              onPressed: displayAddBookClub, icon: const Icon(Icons.add)),
+              onPressed: () async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    insetPadding: const EdgeInsets.all(10),
+                    contentPadding: const EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusDirectional.circular(11)),
+                    backgroundColor: const Color.fromARGB(255, 210, 241, 228),
+                    content: Stack(
+                      children: [
+                        SizedBox(
+                          height: 215,
+                          width: 320,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Cancel',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                        )),
+                                  ),
+                                 /* TextButton(
+                                    child: const Text('Submit',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          color: Color.fromARGB(255, 0, 0, 0),
+                                        )),
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .push(CupertinoPageRoute(
+                                        builder: (context) =>
+                                            const ChooseBookScreen(),
+                                      ));
+                                    },
+                                  )*/
+                                ],
+                              ),
+                              const Text(
+                                'Create a new book club',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 10),
+                              CustomTextUpdate(
+                                label: 'Name',
+                                controller: clubNameController,
+                                validator: (value) {
+                                  return null;
+                                },
+                              ),
+                              const SizedBox(height: 10),
+                              CustomTextUpdate(
+                                label: 'Description',
+                                controller: clubDescriptionController,
+                                validator: (value) {
+                                  return null;
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add)),
         ), // need to find a good place for this
         Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -45,8 +136,7 @@ class MyBookClubs extends StatelessWidget {
                 child: const BookClubCard(),
                 onPressed: () {
                   Navigator.of(context).push(CupertinoPageRoute(
-                      builder: (context) => const BookClubScreen()
-                      ));
+                      builder: (context) => const BookClubScreen()));
                 },
               ),
             ])
