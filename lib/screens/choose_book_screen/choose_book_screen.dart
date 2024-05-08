9507a27fef91a491593afcee58508a8192fe7b3d
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-08 17:30:38 -0400

    added listview rendering for search results

--- lib/screens/choose_book_screen/choose_book_screen.dart
+++ lib/screens/choose_book_screen/choose_book_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/api/book_service.dart';
+import 'package:papyrus/core/models/book.dart';
 import 'package:papyrus/core/models/book_club.dart';
 import 'package:papyrus/screens/widgets/book_card.dart';
 import 'package:papyrus/screens/widgets/book_timeline.dart';
@@ -16,51 +18,67 @@ BookClub bookClub = BookClub(
 );
 
 class ChooseBookScreen extends StatefulWidget {
-  const ChooseBookScreen({super.key});
+  const ChooseBookScreen({Key? key}) : super(key: key);
 
   @override
   State<ChooseBookScreen> createState() => _ChooseBookScreenState();
 }
 
 class _ChooseBookScreenState extends State<ChooseBookScreen> {
-  //final _formKey = GlobalKey<FormState>();
+  TextEditingController searchController = TextEditingController();
+  BookService bookService = BookService();
+  List<Book> searchResults = [];
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
           CupertinoNavigationBar(
-            middle: const Text("Choose a book for your club",
-                style: TextStyle(
-                    color: Color.fromRGBO(245, 245, 221, 1),
-                    fontFamily: 'Inter',
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold)),
+            middle: const Text(
+              "Choose a book for your club",
+              style: TextStyle(
+                color: Color.fromRGBO(245, 245, 221, 1),
+                fontFamily: 'Inter',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             leading: IconButton(
               icon: const Icon(Icons.arrow_back_ios, color: Colors.grey),
               onPressed: () => Navigator.of(context).pop(),
             ),
             backgroundColor: const Color(0xFF001A23),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const BookCard(),
-              Align(
-                  child: SizedBox(
-                width: 367,
-                height: 90,
-                child: Row(children: [
-                  DescriptionBox(description: bookClub.description),
-                  const InformationBox()
-                ]),
-              )),
-              const ReadingProgress(),
-              const BookTimeline(),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search for a book by title or ISBN',
+              ),
+              onChanged: (value) async {
+                if (value.isNotEmpty) {
+                  final results = await bookService.searchBooksByQuery(value);
+                  setState(() {
+                    searchResults = results;
+                  });
+                } else {
+                  setState(() {
+                    searchResults = [];
+                  });
+                }
+              },
+            ),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults.length,
+              itemBuilder: (context, index) {
+                return BookCard();
+              },
+            ),
+          )
         ],
       ),
     );
