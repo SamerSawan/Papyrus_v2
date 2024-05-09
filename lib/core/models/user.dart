744c7d91fbd1b5811f21e6f6f07efe66d63abaf3
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-09 17:20:22 -0400

    added a profile screen

--- lib/core/models/user.dart
+++ lib/core/models/user.dart
@@ -8,6 +8,13 @@ class User {
 
   User({required this.username, required this.email, required this.uid});
 
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      uid: map['uid'],
+      email: map['email'],
+      username: map['username'],
+    );
+  }
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
