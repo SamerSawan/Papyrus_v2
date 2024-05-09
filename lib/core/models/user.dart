d07636b547d00b046fe9777d3e86cb204c654e2e
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-09 12:06:55 -0400

    minor update to user model, gave bookClubs a default value

--- lib/core/models/user.dart
+++ lib/core/models/user.dart
@@ -3,7 +3,7 @@ import 'package:papyrus/core/models/book_club.dart';
 class User {
   final String username;
   final String email;
-  final List<BookClub> bookClubs;
+  final List<BookClub> bookClubs = [];
 
-  User({required this.username, required this.email, required this.bookClubs});
+  User({required this.username, required this.email});
 }
