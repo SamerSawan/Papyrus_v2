98715f2661cde637e9506234be1439e75ae62611
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-08 20:35:10 -0400

    adjusted choosebookscreen to properly display the searched books

--- lib/screens/book_club_screen/book_club_screen.dart
+++ lib/screens/book_club_screen/book_club_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/api/book_service.dart';
+import 'package:papyrus/core/models/book.dart';
 import 'package:papyrus/core/models/book_club.dart';
 import 'package:papyrus/screens/widgets/book_card.dart';
 import 'package:papyrus/screens/widgets/book_timeline.dart';
@@ -23,46 +25,75 @@ class BookClubScreen extends StatefulWidget {
 }
 
 class _BookClubScreenState extends State<BookClubScreen> {
-  //final _formKey = GlobalKey<FormState>();
+  BookService bookService = BookService();
+  late Future<Book> book = bookService.findBook(bookTitle: "Anna Karenina");
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: const TestPop(),
-      body: ListView(children: [
-        CupertinoNavigationBar(
-          middle: Text(bookClub.name,
-              style: const TextStyle(
-                  color: Color.fromRGBO(245, 245, 221, 1),
-                  fontFamily: 'Inter',
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold)),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.grey),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          backgroundColor: const Color(0xFF001A23),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const BookCard(),
-            Align(
-                child: SizedBox(
-              width: 367,
-              height: 90,
-              child: Row(children: [
-                DescriptionBox(description: bookClub.description),
-                const InformationBox()
-              ]),
-            )),
-            const ReadingProgress(),
-            const BookTimeline(),
-          ],
-        ),
-      ]),
+      body: FutureBuilder<Book>(
+        future: book,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // While the book is loading, show a loading indicator
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            // If there's an error fetching the book, display an error message
+            return Center(
+              child: Text('Error loading book: ${snapshot.error}'),
+            );
+          } else {
+            // If the book is successfully loaded, display the BookCard
+            return ListView(
+              children: [
+                CupertinoNavigationBar(
+                  middle: Text(
+                    bookClub.name,
+                    style: const TextStyle(
+                      color: Color.fromRGBO(245, 245, 221, 1),
+                      fontFamily: 'Inter',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios, color: Colors.grey),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  backgroundColor: const Color(0xFF001A23),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    BookCard(
+                        book: snapshot
+                            .data!), // Use snapshot.data to access the loaded book
+                    Align(
+                      child: SizedBox(
+                        width: 367,
+                        height: 90,
+                        child: Row(
+                          children: [
+                            DescriptionBox(description: bookClub.description),
+                            const InformationBox(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const ReadingProgress(),
+                    const BookTimeline(),
+                  ],
+                ),
+              ],
+            );
+          }
+        },
+      ),
     );
   }
 }
