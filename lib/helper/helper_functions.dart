b1b1c3c6cf4be2beac45917d6a11bc787b258197
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-10 19:56:28 -0400

    functional invite button is now available

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/core/api/firestore_service.dart';
+import 'package:papyrus/core/api/test.dart';
+import 'package:papyrus/core/models/book_club.dart';
+import 'package:papyrus/screens/widgets/custom_text_input.dart';
 
 void displayMessageToUser(String message, BuildContext context) {
   showDialog(
@@ -24,5 +28,74 @@ void displayAddBookClub(BuildContext context) async {
             )
           ],
         )),
+  );
+}
+
+void displayInviteUser(BuildContext context, BookClub bookClub) {
+  TextEditingController usernameController = TextEditingController();
+  FirestoreService firestoreService = FirestoreService();
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      insetPadding: const EdgeInsets.all(10),
+      contentPadding: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusDirectional.circular(11)),
+      backgroundColor: const Color.fromARGB(255, 210, 241, 228),
+      content: Stack(
+        children: [
+          SizedBox(
+            height: 150,
+            width: 320,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          )),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        firestoreService.inviteUser(
+                            username: usernameController.text,
+                            bookClub: bookClub);
+                      },
+                      child: const Text('Confirm',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          )),
+                    )
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "Enter username",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                CustomTextInput(
+                  controller: usernameController,
+                  label: '',
+                  validator: (String? value) {},
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
   );
 }
