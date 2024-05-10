fe0b0fbb13fa653fa0826df9a59a68a48fd2b70d
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-10 14:23:52 -0400

    modified bookclubs list in user to be the id of the book club once it is created in firebase, and also render them correctly

---
+++ lib/core/models/invite.dart
@@ -0,0 +1,8 @@
+import 'package:papyrus/core/models/book_club.dart';
+
+class Invite {
+  final String inviteID;
+  final BookClub bookClub;
+
+  Invite({required this.inviteID, required this.bookClub});
+}
