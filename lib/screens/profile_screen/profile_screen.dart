13838f7539645e1a1c97e013e9bdc2aafeee3ee2
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-10 01:01:24 -0400

    Added a functional notifications button in profile

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,7 +1,9 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_auth/firebase_auth.dart" as auth;
+import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:papyrus/core/models/user.dart";
+import "package:papyrus/screens/notifications_screen/notifications_screen.dart";
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({super.key});
@@ -18,8 +20,25 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Profile"),
+        appBar: CupertinoNavigationBar(
+          backgroundColor: const Color(0xFF001A23),
+          middle: const Text(
+            "Profile",
+            style: TextStyle(
+              color: Color(0xFFF5F5DD),
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationsScreen()),
+              );
+            },
+          ),
         ),
         body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
           future: getUserDetails(),
@@ -32,8 +51,10 @@ class ProfileScreen extends StatelessWidget {
               return Text("Error: ${snapshot.error}");
             } else if (snapshot.hasData) {
               User user = User.fromMap(snapshot.data!.data()!);
-              return Column(
-                children: [Text(user.email), Text(user.username)],
+              return Center(
+                child: Column(
+                  children: [Text(user.email), Text(user.username)],
+                ),
               );
             } else {
               return Text("No data");
