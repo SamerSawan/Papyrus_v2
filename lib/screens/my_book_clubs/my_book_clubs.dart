5bc5d62e9d3da947e644bff6ef02a272f738d793
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-09 22:00:32 -0400

    Bookclubs are now automatically rendered

--- lib/screens/my_book_clubs/my_book_clubs.dart
+++ lib/screens/my_book_clubs/my_book_clubs.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,25 +10,8 @@ import 'package:papyrus/screens/widgets/book_club_card.dart';
 import 'package:papyrus/screens/widgets/custom_text_update.dart';
 import 'package:papyrus/screens/widgets/search_bar_clubs.dart';
 
-BookClub bookClub = BookClub(
-  name: "Shareholder Pleasers",
-  currentBook: Book(
-    title: "Anna Karenina",
-    authors: ["Leo Tolstoy"],
-    isbns: ["1"],
-    description:
-        "Love... it means too much to me, far more than you can understand.At its simplest, Anna Karenina is a love story. It is a portrait of a beautiful and intelligent woman whose passionate love for a handsome officer sweeps aside all other ties - to her marriage and to the network of relationships and moral values that bind the society around her. The love affair of Anna and Vronsky is played out alongside the developingromance of Kitty and Levin, and in the character of Levin, closely based on Tolstoy himself, the search for happiness takes on a deeper philosophical significance. One of the greatest novels ever written,Anna Karenina combines penetrating psychological insight with",
-    pageCount: 896,
-    categories: [],
-    image:
-        'https://books.google.ca/books?id=1DooDwAAQBAJ&printsec=frontcover&source=gbs_ge_summary_r&cad=0',
-  ),
-  description: 'silly group',
-  users: [],
-);
-
 class MyBookClubs extends StatefulWidget {
-  const MyBookClubs({super.key});
+  const MyBookClubs({Key? key}) : super(key: key);
 
   @override
   State<MyBookClubs> createState() => _MyBookClubsState();
@@ -41,133 +25,159 @@ class _MyBookClubsState extends State<MyBookClubs> {
     FirebaseAuth.instance.signOut();
   }
 
+  Future<List<BookClub>> getBookClubs() async {
+    User? currentUser = FirebaseAuth.instance.currentUser;
+    final docRef =
+        FirebaseFirestore.instance.collection("Users").doc(currentUser?.email);
+    final DocumentSnapshot doc = await docRef.get();
+    final data = doc.data() as Map<String, dynamic>;
+    List<BookClub> bookClubs = data['bookClubs'].map<BookClub>((clubMap) {
+      return BookClub.fromMap(clubMap);
+    }).toList();
+    return bookClubs;
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
-      body: ListView(children: [
-        CupertinoNavigationBar(
-          middle: const Text(
-            'My Book Clubs',
-            style: TextStyle(
-              color: Color(0xFFF5F5DD),
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+      appBar: CupertinoNavigationBar(
+        middle: const Text(
+          'My Book Clubs',
+          style: TextStyle(
+            color: Color(0xFFF5F5DD),
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        backgroundColor: const Color(0xFF001A23),
+        leading: IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
+        trailing: IconButton(
+            onPressed: () {
+              showDialog<void>(
+                context: context,
+                builder: (context) => AlertDialog(
+                  insetPadding: const EdgeInsets.all(10),
+                  contentPadding: const EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusDirectional.circular(11)),
+                  backgroundColor: const Color.fromARGB(255, 210, 241, 228),
+                  content: Stack(
+                    children: [
+                      SizedBox(
+                        height: 215,
+                        width: 320,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Cancel',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(255, 0, 0, 0),
+                                      )),
+                                ),
+                                TextButton(
+                                  child: const Text('Submit',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(255, 0, 0, 0),
+                                      )),
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .push(CupertinoPageRoute(
+                                      builder: (context) => ChooseBookScreen(
+                                          bookClubName: clubNameController.text,
+                                          description:
+                                              clubDescriptionController.text),
+                                    ));
+                                  },
+                                )
+                              ],
+                            ),
+                            const Text(
+                              'Create a new book club',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            CustomTextUpdate(
+                              label: 'Name',
+                              controller: clubNameController,
+                              validator: (value) {
+                                return null;
+                              },
+                              height: 35,
+                            ),
+                            const SizedBox(height: 10),
+                            CustomTextUpdate(
+                              label: 'Description',
+                              controller: clubDescriptionController,
+                              validator: (value) {
+                                return null;
+                              },
+                              height: 75,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add)),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: 367,
+            child: SearchTextField(
+              searchController: TextEditingController(),
+              hintText: "Search by title or ISBN",
+              onChanged: (String) {},
             ),
           ),
-          backgroundColor: const Color(0xFF001A23),
-          leading:
-              IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
-          trailing: IconButton(
-              onPressed: () {
-                showDialog<void>(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    insetPadding: const EdgeInsets.all(10),
-                    contentPadding: const EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusDirectional.circular(11)),
-                    backgroundColor: const Color.fromARGB(255, 210, 241, 228),
-                    content: Stack(
-                      children: [
-                        SizedBox(
-                          height: 215,
-                          width: 320,
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('Cancel',
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: Color.fromARGB(255, 0, 0, 0),
-                                        )),
-                                  ),
-                                  TextButton(
-                                    child: const Text('Submit',
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: Color.fromARGB(255, 0, 0, 0),
-                                        )),
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .push(CupertinoPageRoute(
-                                        builder: (context) => ChooseBookScreen(
-                                            bookClubName:
-                                                clubNameController.text,
-                                            description:
-                                                clubDescriptionController.text),
-                                      ));
-                                    },
-                                  )
-                                ],
+          Expanded(
+            child: FutureBuilder<List<BookClub>>(
+              future: getBookClubs(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CupertinoActivityIndicator());
+                } else if (snapshot.hasError || snapshot.data == null) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final bookClub = snapshot.data![index];
+                      return CupertinoButton(
+                        child: BookClubCard(bookClub: bookClub),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            CupertinoPageRoute(
+                              builder: (context) => BookClubScreen(
+                                bookClub: bookClub,
                               ),
-                              const Text(
-                                'Create a new book club',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(height: 10),
-                              CustomTextUpdate(
-                                label: 'Name',
-                                controller: clubNameController,
-                                validator: (value) {
-                                  return null;
-                                },
-                                height: 35,
-                              ),
-                              const SizedBox(height: 10),
-                              CustomTextUpdate(
-                                label: 'Description',
-                                controller: clubDescriptionController,
-                                validator: (value) {
-                                  return null;
-                                },
-                                height: 75,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.add)),
-        ), // need to find a good place for this
-        Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                  width: 367,
-                  child: SearchTextField(
-                    searchController: TextEditingController(),
-                    hintText: "Search by title or ISBN",
-                    onChanged: (String) {},
-                  )),
-              CupertinoButton(
-                child: const BookClubCard(),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    CupertinoPageRoute(
-                      builder: (context) => BookClubScreen(
-                        bookClub: bookClub,
-                      ),
-                    ),
+                            ),
+                          );
+                        },
+                      );
+                    },
                   );
-                },
-              ),
-            ])
-      ]),
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/widgets/book_club_card.dart
+++ lib/screens/widgets/book_club_card.dart
@@ -4,141 +4,105 @@ import 'package:papyrus/core/models/book.dart';
 import 'package:papyrus/core/models/book_club.dart';
 import 'package:papyrus/screens/widgets/book_club_title.dart';
 
-BookClub bookClub = BookClub(
-  name: "Shareholder Pleasers",
-  currentBook: Book(
-    title: "Anna Karenina",
-    authors: ["Leo Tolstoy"],
-    isbns: ["1"],
-    description:
-        "Love... it means too much to me, far more than you can understand.At its simplest, Anna Karenina is a love story. It is a portrait of a beautiful and intelligent woman whose passionate love for a handsome officer sweeps aside all other ties - to her marriage and to the network of relationships and moral values that bind the society around her. The love affair of Anna and Vronsky is played out alongside the developingromance of Kitty and Levin, and in the character of Levin, closely based on Tolstoy himself, the search for happiness takes on a deeper philosophical significance. One of the greatest novels ever written,Anna Karenina combines penetrating psychological insight with",
-    pageCount: 896,
-    categories: [],
-    image:
-        'https://books.google.ca/books?id=1DooDwAAQBAJ&printsec=frontcover&source=gbs_ge_summary_r&cad=0',
-  ),
-  description: 'silly group',
-  users: [],
-);
-
 class BookClubCard extends StatefulWidget {
-  const BookClubCard({super.key});
+  final BookClub bookClub;
+  const BookClubCard({super.key, required this.bookClub});
 
   @override
   State<BookClubCard> createState() => _BookClubCardState();
 }
 
 class _BookClubCardState extends State<BookClubCard> {
-  late Future<Book> futureBook;
-  BookService bookService = BookService();
-
-  @override
-  void initState() {
-    super.initState();
-    futureBook = bookService.findBook(bookTitle: 'Anna Karenina');
-  }
-
   @override
   Widget build(context) {
-    return Column(children: [
-      SizedBox(
+    return Column(
+      children: [
+        SizedBox(
           width: 367,
-          child: Column(children: [
-            Container(
+          child: Column(
+            children: [
+              Container(
                 width: 367.0,
                 height: 150.0,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(6),
-                    color: const Color(0xFF53917E).withOpacity(0.5)),
-                child: FutureBuilder(
-                  future: futureBook,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final book = snapshot.data;
-                      if (book != null) {
-                        return Row(children: [
-                          const SizedBox(width: 15),
-                          Container(
-                            height: 127,
-                            width: 80,
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8)),
-                            child: Image.network(book.image),
+                    color: Color.fromRGBO(83, 145, 126, 1).withOpacity(0.5)),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 15),
+                    Container(
+                      height: 127,
+                      width: 80,
+                      clipBehavior: Clip.hardEdge,
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(8)),
+                      child: Image.network(widget.bookClub.currentBook.image),
+                    ),
+                    const SizedBox(width: 15),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 21),
+                          BookClubTitle(name: widget.bookClub.name),
+                          SizedBox(
+                            width: 200,
+                            child: Text(
+                              '${widget.bookClub.currentBook.title} by ${widget.bookClub.currentBook.authors[0]}',
+                              style: const TextStyle(
+                                color: Color(0xFFF5F5DD),
+                                fontFamily: 'Inter',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
                           ),
-                          const SizedBox(width: 15),
-                          Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 21),
-                                BookClubTitle(name: bookClub.name),
-                                Text(
-                                  '${bookClub.currentBook.title} by ${book.authors[0]}',
-                                  style: const TextStyle(
-                                    color: Color(0xFFF5F5DD),
-                                    fontFamily: 'Inter',
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                                const Text(
-                                  'Ratings',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color.fromRGBO(245, 245, 221, 1),
-                                    fontFamily: 'Inter',
-                                    fontSize: 10,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                const Text(
-                                  'Created by [User]',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color.fromRGBO(245, 245, 221, 1),
-                                    fontFamily: 'Inter',
-                                    fontSize: 10,
-                                  ),
-                                ),
-                                const Text(
-                                  'Participants : #',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color.fromRGBO(245, 245, 221, 1),
-                                    fontFamily: 'Inter',
-                                    fontSize: 10,
-                                  ),
-                                ),
-                                const Text(
-                                  'Date Started: DD/MM/YY',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Color.fromRGBO(245, 245, 221, 1),
-                                    fontFamily: 'Inter',
-                                    fontSize: 10,
-                                  ),
-                                ),
-                              ])
-                        ]);
-                      } else {
-                        return const Text('No Book Data');
-                      }
-                    } else if (snapshot.hasError) {
-                      return Text('$snapshot.error');
-                    }
-                    return const Center(
-                        child: SizedBox(
-                            width: 30,
-                            height: 30,
-                            child: CircularProgressIndicator(
-                              color: Color.fromRGBO(245, 245, 221, 0.464),
-                              strokeWidth: 1,
-                            )));
-                  },
-                ))
-          ]))
-    ]);
+                          const Text(
+                            'Ratings',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromRGBO(245, 245, 221, 1),
+                              fontFamily: 'Inter',
+                              fontSize: 10,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 2,
+                          ),
+                          const Text(
+                            'Created by [User]',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromRGBO(245, 245, 221, 1),
+                              fontFamily: 'Inter',
+                              fontSize: 10,
+                            ),
+                          ),
+                          const Text(
+                            'Participants : #',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromRGBO(245, 245, 221, 1),
+                              fontFamily: 'Inter',
+                              fontSize: 10,
+                            ),
+                          ),
+                          const Text(
+                            'Date Started: DD/MM/YY',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromRGBO(245, 245, 221, 1),
+                              fontFamily: 'Inter',
+                              fontSize: 10,
+                            ),
+                          ),
+                        ])
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
