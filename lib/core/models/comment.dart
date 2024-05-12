a4fabf6a49f07b5988977d4a14f3ec7a3e187eae
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2024-05-12 08:23:04 -0400

    created a new comment class that is seperated from any other functionality

---
+++ lib/core/models/comment.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:papyrus/core/models/book.dart';
+
+class Comment {
+  final String comment;
+  final String username;
+  final Text timestamp;
+  final num percentage;
+  final String commentId;
+  final List<String> likes;
+
+  const Comment({
+    required this.comment,
+    required this.username,
+    required this.timestamp,
+    required this.percentage,
+    required this.commentId,
+    required this.likes,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'comment': comment,
+      'username': username,
+      'timestamp': timestamp.data,
+      'percentage': percentage,
+      'commentId': commentId,
+      'likes': likes,
+    };
+  }
+
+  static Comment fromMap(Map<String, dynamic> map) {
+    return Comment(
+      comment: map['comment'],
+      username: map['username'],
+      timestamp: Text(map['timestamp']),
+      percentage: map['percentage'],
+      commentId: map['commentId'],
+      likes: List<String>.from(map['likes']),
+    );
+  }
+}
--- lib/screens/widgets/comment.dart
+++ lib/screens/widgets/comment.dart
@@ -3,11 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:papyrus/core/api/book_service.dart';
 import 'package:papyrus/core/models/book.dart';
-import 'package:papyrus/core/models/user.dart';
 import 'package:papyrus/core/models/book_club.dart';
 import 'package:papyrus/screens/widgets/like_button.dart';
-import 'package:papyrus/core/api/firestore_service.dart';
-
 
 BookClub bookClub = BookClub(
   name: "Shareholder Pleasers",
@@ -26,8 +23,7 @@ BookClub bookClub = BookClub(
   users: [],
 );
 
-
-class CommentBox extends StatefulWidget{
+class CommentBox extends StatefulWidget {
   final Book book;
   final String comment;
   final String username;
@@ -36,32 +32,30 @@ class CommentBox extends StatefulWidget{
   final String commentId;
   final List<String> likes;
 
-  const CommentBox ({
-    super.key, 
-    required this.book,
-    required this.comment, 
-    required this.username,
-    required this.timestamp, 
-    required this.percentage,
-    required this.commentId,
-    required this.likes
-    });
+  const CommentBox(
+      {super.key,
+      required this.book,
+      required this.comment,
+      required this.username,
+      required this.timestamp,
+      required this.percentage,
+      required this.commentId,
+      required this.likes});
 
   @override
   State<CommentBox> createState() => _CommentBoxState();
 }
 
 class _CommentBoxState extends State<CommentBox> {
-
-bool isLiked = false;
-BookService bookService = BookService();
-late Book book;
-final currentUser = auth.FirebaseAuth.instance.currentUser!;
+  bool isLiked = false;
+  BookService bookService = BookService();
+  late Book book;
+  final currentUser = auth.FirebaseAuth.instance.currentUser!;
 
   @override
   void initState() {
     super.initState();
-    isLiked = widget.likes.contains(currentUser.email); 
+    isLiked = widget.likes.contains(currentUser.email);
     book = widget.book; // Access book from widget
   }
 
@@ -70,90 +64,91 @@ final currentUser = auth.FirebaseAuth.instance.currentUser!;
       isLiked = !isLiked;
     });
 
-  DocumentReference commentRef =
-    FirebaseFirestore.instance.collection('comments').doc(widget.commentId);
+    DocumentReference commentRef =
+        FirebaseFirestore.instance.collection('comments').doc(widget.commentId);
 
-      if (isLiked){
-        commentRef.update({
-          'Likes' : FieldValue.arrayUnion([currentUser.email])
-        });
-      }
-      else {
-        commentRef.update({
-         'Likes' : FieldValue.arrayRemove([currentUser.email])
-        });
-      }
+    if (isLiked) {
+      commentRef.update({
+        'Likes': FieldValue.arrayUnion([currentUser.email])
+      });
+    } else {
+      commentRef.update({
+        'Likes': FieldValue.arrayRemove([currentUser.email])
+      });
+    }
   }
 
   @override
   Widget build(context) {
     return Container(
-                padding: const EdgeInsets.all(10),
-                width: 367,
-                color: Theme.of(context).colorScheme.primary,
-                child: Stack(
-                  children: [Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Theme.of(context).colorScheme.secondary,
-                            radius: 15,
-                            child: const Text('JC'),
-                            ),
-                          const SizedBox(width: 10),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                              '${widget.username} is ${widget.percentage} % through ${book.title}',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            widget.timestamp,
-                        ]),
-                        ],
-                      ),
-                      const SizedBox(height: 5),
-                      Text(widget.comment, style: Theme.of(context).textTheme.bodySmall),
-                        ],
-                      ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Icon(
-                          Icons.delete,
-                          color: Theme.of(context).primaryIconTheme.color,
-                          size: Theme.of(context).primaryIconTheme.size,
+        padding: const EdgeInsets.all(10),
+        width: 367,
+        color: Theme.of(context).colorScheme.primary,
+        child: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                    radius: 15,
+                    child: const Text('JC'),
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${widget.username} is ${widget.percentage} % through ${book.title}',
+                          style: Theme.of(context).textTheme.bodyMedium,
                         ),
-                      ), // added delete icon
-                      Align(
-                        alignment: const Alignment(1,1),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                LikeButton(isLiked: isLiked, onTap: toggleLike), //like button
-                                Text( // number of likes
-                                  widget.likes.length.toString(),
-                                  style: Theme.of(context).textTheme.bodySmall,
-                                )
-                              ],
-                            ),
-                            const SizedBox(width: 5),
-                            Icon(
-                              Icons.chat_bubble_outline_rounded, // need to change comment icon i dont like this one lol
-                              color: Theme.of(context).primaryIconTheme.color,
-                              size: Theme.of(context).primaryIconTheme.size,
-                                )
-                              ],
-                            )
-                          ),
-                ])
-              );
+                        widget.timestamp,
+                      ]),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Text(widget.comment,
+                  style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Icon(
+              Icons.delete,
+              color: Theme.of(context).primaryIconTheme.color,
+              size: Theme.of(context).primaryIconTheme.size,
+            ),
+          ), // added delete icon
+          Align(
+              alignment: const Alignment(1, 1),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      LikeButton(
+                          isLiked: isLiked, onTap: toggleLike), //like button
+                      Text(
+                        // number of likes
+                        widget.likes.length.toString(),
+                        style: Theme.of(context).textTheme.bodySmall,
+                      )
+                    ],
+                  ),
+                  const SizedBox(width: 5),
+                  Icon(
+                    Icons
+                        .chat_bubble_outline_rounded, // need to change comment icon i dont like this one lol
+                    color: Theme.of(context).primaryIconTheme.color,
+                    size: Theme.of(context).primaryIconTheme.size,
+                  )
+                ],
+              )),
+        ]));
   }
 }
